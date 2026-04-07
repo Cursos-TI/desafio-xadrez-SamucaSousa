7dd97478afd168378130521b921dfa36b8b405e9
SamucaDev <ss9632771@gmail.com>
2026-04-07 12:00:19 +0000

    Desafio Novato - Estrutura de Repetição

--- xadrez.c
+++ xadrez.c
@@ -7,15 +7,45 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int casasTorre = 5;
+    int casasBispo = 5;
+    int casasRainha = 8;
+
+    // Implementação de Movimentação do Torre
+    // Peça Torre | Movimentos: 5 casas para direita
+    // Estrutura escolhida: FOR (Ideal quando sabemos o numero exato de repetições)
+
+    printf("Movimento da Torre:\n");
+    for(int i = 1; i <= casasTorre; i++){
+        printf("{Direta} ");
+    };
+    printf("\n");
 
     // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // Peça: Bispo | Movimento: 5 casas para Cima e Direita (Diagonal)
+    // Estrutura escolhida: WHILE (executa enquanto a condição for verdadeira)
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("Movimento do Bispo:\n");
+    int contadorBispo = 1;
+
+    while(contadorBispo <= casasBispo){
+        printf("{Cima} {Direita} ");
+        contadorBispo++;
+    }
+    printf("\n");
 
     // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // Peça: Rainha | Movimento: 8 casas para Esquerda
+    // Estrutura escolhida: WHILE (executa enquanto a condição for verdadeira)
+
+    printf("Movimento da Rainha:\n");
+    int contadorRainha = 1;
+    
+    do{
+        printf("{Esquerda} ");
+        contadorRainha++;
+    } while (contadorRainha <= casasRainha);
+
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
